34d1ae28dd77ac86121e6fe0df2a7a41a29b07c2
shnynzn <116573561+shnynzn@users.noreply.github.com>
2023-04-13 19:37:34 +0800

    Update GradCam.py

--- grc/GradCam.py
+++ grc/GradCam.py
@@ -21,7 +21,11 @@ def train_step(features, labels):
 
         if self.layerName == None:
             self.layerName = self.find_target_layer()"""
-
+'''def compute_heatmap(self, image, classIdx, upsample_size, eps=1e-5):
+        gradModel = Model(
+            inputs=[self.model.inputs],
+            outputs=[self.model.get_layer(self.layerName).output, self.model.output]
+        )'''
 
 import tensorflow as tf
 import cv2
@@ -30,15 +34,15 @@ from tensorflow.keras.models import Model
 
 class GradCAM:
     def __init__(self, model, classIdx, inner_model=None, layerName=None):
-        self.model = model
+        self.model = model    
         self.classIdx = classIdx
         self.inner_model = inner_model
+            if self.inner_model == None:
+                self.inner_model = self.model
         
-        if self.inner_model == None:
-            self.inner_model = model
-        
-        if self.layerName == None:
-            self.layerName = self.find_target_layer()
+        self.layerName = layerName
+            if self.layerName == None:
+                self.layerName = self.find_target_layer()
 
     def find_target_layer(self):
         for layer in reversed(self.model.layers):
@@ -46,11 +50,6 @@ class GradCAM:
                 return layer.name
         raise ValueError("Could not find 4D layer. Cannot apply GradCAM")
 
-    '''def compute_heatmap(self, image, classIdx, upsample_size, eps=1e-5):
-        gradModel = Model(
-            inputs=[self.model.inputs],
-            outputs=[self.model.get_layer(self.layerName).output, self.model.output]
-        )'''
         # record operations for automatic differentiation
     def compute_heatmap(self, image, classIdx, upsample_size, eps=1e-5):
         gradModel = Model(
