13283db0d86c9d109ba2c9485e7d10870d889e0d
shnynzn <116573561+shnynzn@users.noreply.github.com>
2023-04-14 14:47:53 +0800

    Update GradCam.py

--- grc/GradCam.py
+++ grc/GradCam.py
@@ -56,7 +56,7 @@ class GradCAM:
     def compute_heatmap(self, image, classIdx, upsample_size, eps=1e-5):
         gradModel = Model(
             inputs=[self.inner_model.inputs],
-            outputs=[self.model.get_layer(self.layerName).output,
+            outputs=[self.model.(self.layerName).output,
                      self.model.output]) 
         
         with tf.GradientTape() as tape:
